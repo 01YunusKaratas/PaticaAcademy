18e2669bad1eb8cf3441344eb9ecd20ba8fcbfc6
YUNUS EMRE KARATAS <130181127+01YunusKaratas@users.noreply.github.com>
2024-07-02 01:20:54 +0300

    Add files via upload

---
+++ java dersleri/PowerOfTwo.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class PowerOfTwo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num;
+
+        System.out.println("Enter the number :");
+        num = sc.nextInt();
+
+        for (int i = 0, j = 0; (i < num && j < num);) {
+            i += 4;
+            j += 5;
+            System.out.println("i: " + i + ", j: " + j);
+        }
+
+        sc.close();
+    }
+}
