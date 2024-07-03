f6b51700f1dd9766d4f431cba6f0d01a94fb8eab
YUNUS EMRE KARATAS <130181127+01YunusKaratas@users.noreply.github.com>
2024-07-03 23:34:57 +0300

    Add files via upload

---
+++ java dersleri/CombinationAndFactoriel.java
@@ -0,0 +1,21 @@
+/**
+ * CombinationAndFactoriel
+ */
+import java.util.Scanner;
+public class CombinationAndFactoriel {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n,r;
+        int i = 1;
+
+        System.out.println("Enter the  n:");
+        n = sc.nextInt();
+        System.out.println("Enter the r:");
+        r =sc.nextInt();
+
+        for(n;n>=i;n--){
+
+        }
+    }
+    
+}
