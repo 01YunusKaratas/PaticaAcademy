79827ffea0cdcbe8b523a8f76255d8e472f9ef44
YUNUS EMRE KARATAS <130181127+01YunusKaratas@users.noreply.github.com>
2024-07-05 00:41:14 +0300

    Update EbobEkok.java

--- java dersleri/EbobEkok.java
+++ java dersleri/EbobEkok.java
@@ -1,10 +1,45 @@
+import java.security.interfaces.ECKey;
 import java.util.Scanner;
 
 public class EbobEkok {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        
+
+        int number1, number2;
+        int ebob = 1;
+        int ekok = 1;
+        while (true) {
+
+            boolean numbers = false;
+
+            while (!numbers) {
+                System.out.println("Enter the number 1 for  ebob or ekok :?");
+                number1 = sc.nextInt();
+
+                System.out.println("Enter the number 2  for  ebob or ekok :?");
+                number2 = sc.nextInt();
+
+                for (int i = 1; i <= number1; i++) {
+                    if (number1 % i == 0 && number2 % i == 0) {
+                        ebob = i;
+
+                    }
+                }
+
+                for (int j = 1; j <= (number1 * number2); j++) {
+                    if (number1 % j == 0 && number2 % j == 0) {
+                        ekok = j;
+
+                    }
+                }
+
+                System.out.println("******************RESULT******************");
+                System.out.println("Ebob :" + ebob);
+                System.out.println("Ekok:" + (number1 * number2) / ebob);
+
+            }
+        }
+
     }
-    
 }
