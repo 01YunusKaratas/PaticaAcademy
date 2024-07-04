c9d0d76bbb86029a049d3587cb45f57c32aa9b4a
YUNUS EMRE KARATAS <130181127+01YunusKaratas@users.noreply.github.com>
2024-07-05 00:58:05 +0300

    Add files via upload

---
+++ java dersleri/OrderNumber.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class OrderNumber {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("How many numbers do you want to add?");
+        int num = input.nextInt();
+        System.out.println("Created array for " + num + " values");
+
+        int[] numbers = new int[num];
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("Enter the number:");
+            int value = input.nextInt();
+            numbers[i] = value; // Değeri doğrudan atama
+            System.out.println((i + 1) + " value added to the array.");
+        }
+
+        // En büyük ve en küçük değeri bulma
+        int max = Arrays.stream(numbers).max().getAsInt(); // JAVA 8 kullanarak max değeri bulma
+        int min = Arrays.stream(numbers).min().getAsInt(); // JAVA 8 kullanarak min değeri bulma
+
+        System.out.println("MIN: " + min);
+        System.out.println("MAX: " + max);
+
+        // Scanner kaynağını kapatma
+        input.close();
+    }
+}
