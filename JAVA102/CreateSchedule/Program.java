619ed3a5268ec3150997db1d383ab297a0b3ec9b
YUNUS EMRE KARATAS <130181127+01YunusKaratas@users.noreply.github.com>
2024-08-01 21:21:17 +0300

    Add files via upload

---
+++ JAVA102/CreateSchedule/Program.java
@@ -0,0 +1,14 @@
+package CreateSchedule;
+
+/**
+ * Program
+ */
+public class Program {
+    Team t1 = new Team();
+
+    public void start() {
+        System.out.println("Welcome the Fıxture Program .");
+        t1.addTeam();
+        t1.showTeam();
+    }
+}
