34b729c780d5ce6a3a808c143830df5c349891a7
YUNUS EMRE KARATAS <130181127+01YunusKaratas@users.noreply.github.com>
2024-08-10 02:51:41 +0300

    Add files via upload

---
+++ JAVA102/Patika-Dev/src/com/patikadev/helper/config.java
@@ -0,0 +1,10 @@
+package com.patikadev.helper;
+
+public class config {
+    public  static final String PROJECT_TITLE ="Patika-Dev";
+    public static final String DB_URL = "jdbc:mysql://localhost/patika";
+    public static final String DB_USER="root";
+    public static final String DB_PASSWORD="";
+
+
+}
